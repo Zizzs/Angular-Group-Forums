df9d4e4184a35f54d8e82e4a2362b14e5f9774ec
gold-mir <miranda.keith.42@gmail.com>
2019-02-05 16:23:37 -0800

    add creation methods for posts and comments to post service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from './services/post.service';
+import { Post } from './models/post.model';
+import { Comment } from './models/comment.model';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +14,20 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
     //fy4BwQ7viua0kOKXkV25
-    this.postService.getComments('fy4BwQ7viua0kOKXkV25').subscribe((data) => {
+    this.postService.getPosts('yV5TsXLEO74NRyxR6uc0').subscribe((data) => {
       console.log(data);
     });
+  }
+
+  async post(){
+    let post = new Post(`Posted at ${Date.now()}`, 'heck', null, 'yV5TsXLEO74NRyxR6uc0');
+    let result = this.postService.createPost(post);
+    console.log(await result);
+  }
+
+  async postComment(){
+    let newComment = new Comment(`Posted at ${Date.now()}`, null, 'fy4BwQ7viua0kOKXkV25', 'kO7GIUojiSMy4buyOWFL');
+    let result = this.postService.createComment(newComment, newComment.postId);
+    console.log(await result);
   }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
 })
 export class PostService {
 
-  private postsCollection: AngularFirestoreCollection<Post>;
+  private postsCollection: AngularFirestoreCollection;
   private posts: Observable<Post[]>;
 
   constructor(private db: AngularFirestore) {
@@ -30,9 +30,9 @@ export class PostService {
     return this.posts;
   }
 
+  //returns a promise that resolves to an observable returning the result of a query
   public getPosts(topicId: string): Observable<Post[]>{
     let topic = this.db.doc(`topics/${topicId}`).ref;
-    console.log(topic);
 
     let query = this.db.collection('posts', ref => ref.where('topic', '==', topic) );
 
@@ -43,7 +43,6 @@ export class PostService {
       }
       return result;
     }));
-
   }
 
   public getPost(postId: string): Observable<Post>{
@@ -71,9 +70,77 @@ export class PostService {
     }));
   }
 
+  //creates a new post in the database, returns a promise that resolves to the ID of the created post
+  public async createPost(post: Post){
+    if(post.id != null){
+      throw new Error("Error: post already created.");
+    }
+
+    let ref = await this.validateRef('topics',post.topic);
+
+    if(ref != null){
+      let promise = this.postsCollection.add({
+        title: post.title,
+        body: post.body,
+        user: post.user,
+        topic: ref
+      });
+  
+      let id = (await promise).id;
+      return id;
+    } else {
+      throw new Error("Error: Can't create post with nonexistent topic reference") ;
+    }
+  }
+
+  //creates a new comment attached to a post, returns a promies that resolves to the id of the created comment
+  public async createComment(comment: Comment, postId: string){
+    console.log(`Creating new comment ${comment} on post ID ${postId}`);
+    //validate parent id if comment has a parent specified
+    let hasParent: boolean = (comment.parentId != null);
+
+    //validate all references
+    let postRefPromise = this.validateRef('posts', postId);
+    let parentRefPromise = hasParent ? this.validateRef(`posts/${postId}/comments`, comment.parentId) : null;
+
+    let postRef = await postRefPromise;
+    let parentRef = await parentRefPromise;
+
+    console.log(`Post reference: ${postRef}`);
+    console.log(`Parent reference: ${parentRef}`);
+
+    if(postRef != null && (parentRef != null || hasParent == false)){
+      console.log('posting comment');
+      let comments = this.db.collection(`posts/${postId}/comments`);
+      let promise = comments.add({
+        body: comment.body,
+        user: comment.user,
+        parent: parentRef ? parentRef : null
+      });
+
+      let id = (await promise).id;
+      return id;
+    } else {
+      throw new Error("Error: can't create comment with bad references");
+    }
+
+  }
+
   private parsePost(action: DocumentChangeAction<any>): Post{
     let data = action.payload.doc.data();
     let id = action.payload.doc.id;
     return new Post(data.title, data.body, data.user, data.topic.id, id);
+  }
+
+  private async validateRef(collection: string, refId: string){
+    let doc = this.db.doc(`${collection}/${refId}`);
+    let promise = doc.ref.get();
+
+    let exists = (await promise).exists;
+    if(exists){
+      return doc.ref;
+    } else {
+      return null;
+    }
   }
 }
