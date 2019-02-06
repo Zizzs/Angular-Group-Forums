bf87a289351c6207dd50011e951d0e8d89fb96f2
Alex Williams and Stuart Gill <alex@>
2019-02-06 14:37:45 -0800

    Linked database and user auth to main program. Added data flow from database.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,8 @@ import { Comment } from './models/comment.model';
 })
 export class AppComponent implements OnInit{
   title = 'Angular-Group-Forums';
-  constructor(private postService: PostService){}
+  constructor(){}
 
   ngOnInit(){
-    //fy4BwQ7viua0kOKXkV25
-    this.postService.getTopics().subscribe((data) => {
-      console.log(data);
-    });
-  }
-
-  async post(){
-    let post = new Post(`Posted at ${Date.now()}`, 'heck', null, 'yV5TsXLEO74NRyxR6uc0');
-    let result = this.postService.createPost(post);
-    console.log(await result);
-  }
-
-  async postComment(){
-    let newComment = new Comment(`Posted at ${Date.now()}`, null, 'fy4BwQ7viua0kOKXkV25', 'kO7GIUojiSMy4buyOWFL');
-    let result = this.postService.createComment(newComment, newComment.postId);
-    console.log(await result);
   }
 }
