bb8eea86ca71451254d0b22ac628ef889f41762b
Alex Williams and Stuart Gill <stuart@>
2019-02-06 16:28:54 -0800

    Added styling on post and comment creation pages. Added functionality for creating posts and creating comments.

--- src/app/new-comment/new-comment.component.ts
+++ src/app/new-comment/new-comment.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../models/post.model';
+import { Comment } from "../models/comment.model";
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { PostService } from '../services/post.service';
 
 @Component({
   selector: 'app-new-comment',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCommentComponent implements OnInit {
 
-  constructor() { }
+  @Input() post: Post;
+  postId;
+
+  constructor(private route: ActivatedRoute, private authService: AuthService, private postService: PostService) { 
+    // this.route.params.forEach((urlParameters) => {
+    //   this.postId = urlParameters['id'];
+    // });
+  }
 
   ngOnInit() {
   }
 
+  makeComment(body) {
+    let comment = new Comment(body, this.authService.userData.displayName, this.post.id);
+    this.postService.createComment(comment, this.post.id);
+  }
 }
+
+
+// export class Comment {
+//   constructor(public body: string, 
+//       public user: string, 
+//       public postId: string, 
+//       public parentId: string = null, 
+//       public id: string = null
+//        //public date: Date
+//   ){}
+     
